bacb37c26e56b96a0997356e13904a8de39c8508
GALBICCIM <godvictory0909@gmail.com>
2025-03-29 17:16:37 +0900

    FEAT: Profile component implementation with map

--- src/components/pages/About/Profile.tsx
+++ src/components/pages/About/Profile.tsx
@@ -2,32 +2,74 @@ import React from "react";
 import useRelativeFontSize from "hooks/useRelativeFontSize";
 import * as Style from "styles/Profile.styled";
 import { Title, Text } from "styles/Global.styled";
-import { PROFILE_DATA } from "constants/CardContent";
+import { TITLE_LIST, INFO_LIST, CERT_LIST, TECH_LIST } from "constants/CardContent";
 
 const Profile: React.FC = () => {
    const titleSize = useRelativeFontSize(48);
-   const subTitleSize = useRelativeFontSize(38);
-   const aboutSize = useRelativeFontSize(36);
+   const labelSize = useRelativeFontSize(38);
+   const contentSize = useRelativeFontSize(34);
+   const dateSize = useRelativeFontSize(20);
+   const langSize = useRelativeFontSize(24);
 
    return (
       <Style.Container>
-         {PROFILE_DATA.title.map((item, index) => {
+         {TITLE_LIST.map((item, index) => {
             return (
-               <Style.Card>
+               <Style.Card key={index}>
                   <Title rem={titleSize} font="NSansBold">
                      {item}
                   </Title>
                   <Style.TextWrapper gap={index === 0 ? 2 : index === 1 ? 10 : 3}>
-                     {PROFILE_DATA.content.map((item, index) => {
-                        return (
-                           <div>
-                              <Text rem={subTitleSize} font="NSansBold">
-                                 {item[index].label}
-                              </Text>
-                              <Text rem={aboutSize} font="NSansRegular"></Text>
-                           </div>
-                        );
-                     })}
+                     {index === 0
+                        ? INFO_LIST.map((info, infoIndex) => (
+                             <div key={infoIndex}>
+                                <Text rem={labelSize} font="NSansBold">
+                                   {info.label}
+                                </Text>
+                                {Array.isArray(info.value) ? (
+                                   info.value.map((infoValueItem) => (
+                                      <div>
+                                         <Text rem={contentSize} font="NSansRegular">
+                                            {infoValueItem.text}
+                                         </Text>
+                                         <Text rem={dateSize} font="NSansRegular" style={{ marginLeft: "16px" }}>
+                                            {infoValueItem.date}
+                                         </Text>
+                                      </div>
+                                   ))
+                                ) : (
+                                   <Text rem={contentSize} font="NSansRegular">
+                                      {info.value}
+                                   </Text>
+                                )}
+                             </div>
+                          ))
+                        : index === 1
+                        ? CERT_LIST.map((cert, certIndex) => (
+                             <div key={certIndex}>
+                                <Text rem={labelSize} font="NSansBold">
+                                   {cert.label}
+                                </Text>
+                                <Text rem={dateSize} font="NSansRegular" style={{ marginLeft: "16px" }}>
+                                   {cert.date}
+                                </Text>
+                             </div>
+                          ))
+                        : TECH_LIST.map((tech, techIndex) => (
+                             <div key={techIndex}>
+                                <Text rem={labelSize} font="NSansBold">
+                                   {tech.label}
+                                </Text>
+                                {tech.value.map((techValueItem) => (
+                                   <Style.Wrapper>
+                                      <Text rem={langSize} font="NSansRegular">
+                                         {techValueItem.text}
+                                      </Text>
+                                      <Style.Lamp color={techValueItem.color} />
+                                   </Style.Wrapper>
+                                ))}
+                             </div>
+                          ))}
                   </Style.TextWrapper>
                </Style.Card>
             );
--- src/constants/CardContent.ts
+++ src/constants/CardContent.ts
@@ -1,22 +1,48 @@
-export const PROFILE_DATA = {
-   title: ["인적 사항", "자격증 취득 내역", "기술 스택"],
-   content: [
-      [
-         { label: "이름", value: "박승원" },
-         { label: "전화번호", value: "010-3158-3683" },
-         { label: "E-mail", value: "godvictory0909@gmail.com" },
-         {
-            label: "학력사항",
-            value: [
-               { text: "- 세일중학교", date: "(2020. 3. 2.~2023. 2. 3.)" },
-               { text: "- 한세사이버보안고등학교", date: "(2023. 3. 2.~" },
-            ],
-         },
+export const TITLE_LIST = ["인적 사항", "자격증 취득 내역", "기술 스택"];
+
+export const INFO_LIST = [
+   { label: "이름", value: "박승원" },
+   { label: "전화번호", value: "010-3158-3683" },
+   { label: "E-mail", value: "godvictory0909@gmail.com" },
+   {
+      label: "학력 사항",
+      value: [
+         { text: "- 세일중학교 졸업", date: "(2020. 3. 2.~2023. 2. 3.)" },
+         { text: "- 한세사이버보안고등학교 재학 중", date: "(2023. 3. 2.)" },
       ],
-      [
-         { label: "정보처리기능사", date: "(2024. 4. 17.)" },
-         { label: "ITQ OA Master", date: "(2023. 11. 9.)" },
-         { label: "GTQ 포토샵 1급", date: "(2023. 9. 15.)" },
+   },
+];
+
+export const CERT_LIST = [
+   { label: "- 정보처리기능사", date: "(2024. 4. 17.)" },
+   { label: "- ITQ OA Master", date: "(2023. 11. 9.)" },
+   { label: "- GTQ 포토샵 1급", date: "(2023. 9. 15.)" },
+];
+
+export const TECH_LIST = [
+   {
+      label: "Language",
+      value: [
+         { text: "- Typescript", color: "#7DFF7D" },
+         { text: "- Javascript", color: "#7DFF7D" },
+         { text: "- Python", color: "#FFFF7D" },
+         { text: "- Java", color: "#FFFF7D" },
+         { text: "- C", color: "#FF7D7D" },
       ],
-   ],
-};
+   },
+   {
+      label: "Framework",
+      value: [
+         { text: "- React.js", color: "#7DFF7D" },
+         { text: "- Spring Boot", color: "#FF7D7D" },
+         { text: "- FastAPI", color: "#FF7D7D" },
+      ],
+   },
+   {
+      label: "Other",
+      value: [
+         { text: "- HTML", color: "#7DFF7D" },
+         { text: "- CSS", color: "#7DFF7D" },
+      ],
+   },
+];
